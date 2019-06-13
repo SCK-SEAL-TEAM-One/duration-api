616b71ac20dc6a66436344acc7bad5a0db6b7b9a
endkarn <end_karn@hotmail.com>
2019-06-13 18:00:46 +0700

    Refactor : ลดการ HardCode ส่วนà¸ใน API

--- src/main/main.go
+++ src/main/main.go
@@ -1,32 +1,12 @@
 package main
 
 import (
+	"../api"
 	"log"
-	"fmt"
 	"net/http"
 )
 
 func main(){
-	http.HandleFunc("/duration", func (w http.ResponseWriter, r *http.Request)  {
-		w.Header().Set("Content-Type","application/json")
-		fmt.Fprintf(w,
-		`{
-		"days":7907,
-		"startFullDate":"Thursday, 16 October 1997",
-		"endFullDate":"Monday, 10 June 2019",
-		"daysToMonths":{
-			"months":259,
-			"days":25
-		},
-		"daysToWeeks":{
-			"weeks":1129,
-			"days":4
-		},
-		"seconds":683164800,
-		"minutes":11386080,
-		"hours":189768
-		}`)
-	})
-
+	http.HandleFunc("/duration", api.DurationBetweenTimes)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
