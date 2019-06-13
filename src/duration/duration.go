a34b49ad3c7c23bcb079cf4a6855dcde4255d01a
uracha <uracha_p@kkumail.com>
2019-06-13 17:54:41 +0700

    Edit : แก้ชื่อ Unit test และแก้ลำดับการส่ง Parameter ของกรณีการแปลงข้อความ json เป็นข้อมูล

--- src/duration/duration.go
+++ src/duration/duration.go
@@ -7,9 +7,11 @@ import (
 func GetYearMonthDayFromRequestDate(datejson string) (year int,month int,day int) {
 	var jsonBlob = []byte(datejson)
 	type Date struct{
-		Year int `json:"year"`
-		Month int `json:"month"`
 		Day int `json:"day"`
+		Month int `json:"month"`
+		Year int `json:"year"`
+		
+		
 	}
 	var date Date
 	_ = json.Unmarshal(jsonBlob,&date)
