369af51bc2499ab87e72e5ed6b151e831f78c3ae
uracha <uracha_p@kkumail.com>
2019-06-14 16:51:44 +0700

    Refactor : ปรับไปใช้ Struct แทน Hard code

--- src/api/apiservice.go
+++ src/api/apiservice.go
@@ -1,25 +1,62 @@
 package api
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
 
+type RequestDate struct {
+	Start YearMonthDay 
+	End YearMonthDay
+}
+
+type YearMonthDay struct {
+	Year int `json:"year"`
+	Month int `json:"month"`
+	Day int `json:"day"`
+}
+
+type MonthDay struct {
+	Months int `json:"months"`
+	Days int `json:"days"`
+}
+
+type WeekDay struct {
+	Weeks int `json:"weeks"`
+	Days int `json:"days"`
+}
+
+type Duration struct {
+	Days int `json:"days"`
+	StartFullDate string `json:"startFullDate"`
+	EndFullDate string `json:"endFullDate"`
+	DaysToMonths MonthDay `json:"daysToMonths"`
+	DaysToWeeks WeekDay `json:"daysToWeeks"`
+	Seconds int `json:"seconds"`
+	Minutes int `json:"minutes"`
+	Hours int `json:"hours"`
+	
+	
+}
+
 func DurationBetweenTimes(writer http.ResponseWriter, request *http.Request)  {
-	_, _ = fmt.Fprint(writer, `{
-		"days":7907,
-		"startFullDate":"Thursday, 16 October 1997",
-		"endFullDate":"Monday, 10 June 2019",
-		"daysToMonths":{
-			"months":259,
-			"days":25
-		},
-		"daysToWeeks":{
-			"weeks":1129,
-			"days":4
-		},
-		"seconds":683164800,
-		"minutes":11386080,
-		"hours":189768
-		}`)
+		duration := Duration{
+			StartFullDate : "Thursday, 16 October 1997",
+			EndFullDate : "Monday, 10 June 2019",
+			DaysToMonths : MonthDay{
+				Months:259,
+				Days:25,
+			},
+			DaysToWeeks: WeekDay{
+				Weeks:1129,
+				Days:4,
+			},
+			Days : 7907,
+			Seconds:683164800,
+			Minutes:11386080,
+			Hours:189768,
+		}
+		durationjson,_ := json.Marshal(duration)
+		fmt.Fprint(writer, string(durationjson))
 }
--- src/api/apiservice_test.go
+++ src/api/apiservice_test.go
@@ -12,23 +12,7 @@ func TestApiDurationBetweenTimesPut_StartDay_16_StartMonth_10_StartYear_1997_End
 	body := strings.NewReader(`{"Start": {"day": 16,"month": 10,"year": 1997},"End": {"day": 10,"month": 6,"year": 2019}}`)
 	request := httptest.NewRequest("GET",url,body)
 	recorder := httptest.NewRecorder()
-	expectedResult := `{
-		"days":7907,
-		"startFullDate":"Thursday, 16 October 1997",
-		"endFullDate":"Monday, 10 June 2019",
-		"daysToMonths":{
-			"months":259,
-			"days":25
-		},
-		"daysToWeeks":{
-			"weeks":1129,
-			"days":4
-		},
-		"seconds":683164800,
-		"minutes":11386080,
-		"hours":189768
-		}`
-
+	expectedResult := `{"days":7907,"startFullDate":"Thursday, 16 October 1997","endFullDate":"Monday, 10 June 2019","daysToMonths":{"months":259,"days":25},"daysToWeeks":{"weeks":1129,"days":4},"seconds":683164800,"minutes":11386080,"hours":189768}`
 	DurationBetweenTimes(recorder,request)
 	response := recorder.Result()
 	responseBodyRaw, _ := ioutil.ReadAll(response.Body)
