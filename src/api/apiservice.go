bd0b997210a120877f6272d22b01991e41049999
endkarn <end_karn@hotmail.com>
2019-06-17 17:03:08 +0700

    Refactor : ปรับส่วนการทำงาน API เปลี่ยนมาใช้ฟังชั่นแทน HardCode

--- src/api/apiservice.go
+++ src/api/apiservice.go
@@ -1,37 +1,37 @@
 package api
 
 import (
+	du "../duration"
 	. "../models"
-	"io/ioutil"
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"net/http"
 )
 
-func DurationBetweenTimes(writer http.ResponseWriter, request *http.Request)  {
-		duration := Duration{
-			StartFullDate : "Thursday, 16 October 1997",
-			EndFullDate : "Monday, 10 June 2019",
-			DaysToMonths : MonthDay{
-				Months:259,
-				Days:25,
-			},
-			DaysToWeeks: WeekDay{
-				Weeks:1129,
-				Days:4,
-			},
-			Days : 7907,
-			Seconds:683164800,
-			Minutes:11386080,
-			Hours:189768,
-		}
-		durationjson,_ := json.Marshal(duration)
-		fmt.Fprint(writer, string(durationjson))
+func DurationBetweenTimes(writer http.ResponseWriter, request *http.Request) {
+	requestDate := GetYearMonthDayFromRequestDate(request)
+	startTime := du.ConvertYearMonthDayToTime(requestDate.Start)
+	endTime := du.ConvertYearMonthDayToTime(requestDate.End)
+	durationValue := du.CalculateDurationBetweenTime(startTime, endTime)
+	totalHours := du.GetHoursFromDuration(durationValue)
+	totalDay := du.ConvertHoursToDays(totalHours)
+	duration := Duration{
+		StartFullDate: du.ConvertTimeToFullDate(startTime),
+		EndFullDate:   du.ConvertTimeToFullDate(endTime),
+		DaysToMonths:  du.CalculateMonthsBetweenStartTimeAndEndTime(requestDate.Start, requestDate.End),
+		DaysToWeeks:   du.ConvertDaysToWeeks(totalDay),
+		Days:          du.ConvertHoursToDays(totalHours),
+		Seconds:       du.GetSecondsFromDuration(durationValue),
+		Minutes:       du.GetMinutesFromDuration(durationValue),
+		Hours:         du.GetHoursFromDuration(durationValue),
+	}
+	durationjson, _ := json.Marshal(duration)
+	fmt.Fprint(writer, string(durationjson))
 }
-
-func GetYearMonthDayFromRequestDate(request *http.Request) RequestDate{
+func GetYearMonthDayFromRequestDate(request *http.Request) RequestDate {
 	body, _ := ioutil.ReadAll(request.Body)
 	var requestDate RequestDate
-	_ = json.Unmarshal(body,&requestDate)
-		return requestDate
+	_ = json.Unmarshal(body, &requestDate)
+	return requestDate
 }
