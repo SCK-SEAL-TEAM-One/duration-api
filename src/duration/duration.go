a757e2889cb8b0124fca65a16a3417439020f4ee
Kanokpron10 <kanokpron@kkumail.com>
2019-06-17 11:53:48 +0700

    Create : สร้างฟังก์ชัน และ Unit test ของ ConvertTimeToFullDate

--- src/duration/duration.go
+++ src/duration/duration.go
@@ -1,5 +1,16 @@
 package duration
 
+import (
+	"fmt"
+	"time"
+)
 
-
-
+func ConvertTimeToFullDate(time time.Time) string {
+	weekDay := time.Weekday()
+	year := time.Year()
+	month := time.Month()
+	day := time.Day()
+	fullDate := fmt.Sprintf("%s, %v %v %v", weekDay, day, month, year)
+	
+	return fullDate
+}
--- src/duration/duration_test.go
+++ src/duration/duration_test.go
@@ -1,1 +1,17 @@
 package duration
+
+import (
+	"time"
+	"testing"
+)
+
+func Test_ConvertTimeToFullDate_Time_Day_16_Month_10_Year_1997_Should_Thursday_16_October_1997(t *testing.T)  {
+	time := time.Date(1997,10,16,0,0,0,0,time.UTC)
+	expectedResult := "Thursday, 16 October 1997"
+
+	actualResult := ConvertTimeToFullDate(time)
+
+	if actualResult != expectedResult {
+		t.Errorf("Expect %v but get %v", expectedResult, actualResult)
+	}
+}
