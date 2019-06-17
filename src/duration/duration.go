b6b5f0a5fb09aafbb8b51130567c9618e7258881
endkarn <end_karn@hotmail.com>
2019-06-17 11:09:42 +0700

    Add : กรณีทดสอบของ CalculateDurationBetweenTime ที่เหลือ

--- src/duration/duration.go
+++ src/duration/duration.go
@@ -3,6 +3,5 @@ package duration
 import "time"
 
 func CalculateDurationBetweenTime(startTime time.Time, endTime time.Time) time.Duration {
-	duration, _ := time.ParseDuration("683164800s")
-	return duration
+	return endTime.Sub(startTime)
 }
