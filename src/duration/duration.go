f9efc0cc575ce6d93c68af5a99322af60fe3c8af
endkarn <end_karn@hotmail.com>
2019-06-17 10:59:51 +0700

    Add : Unit test ของการทดสอบ CalculateDurationBetweenTime

--- src/duration/duration.go
+++ src/duration/duration.go
@@ -1,5 +1,8 @@
 package duration
 
+import "time"
 
-
-
+func CalculateDurationBetweenTime(startTime time.Time, endTime time.Time) time.Duration {
+	duration, _ := time.ParseDuration("683164800s")
+	return duration
+}
--- src/duration/duration_test.go
+++ src/duration/duration_test.go
@@ -1,1 +1,18 @@
 package duration
+
+import (
+	"testing"
+	"time"
+)
+
+func TestCalculateDurationBetweenTime_Year_1997_Month_10_Day_16_And_Year_2019_Month_06_Day_10_Should_Get_Duration_683164800_Secounds(t *testing.T) {
+	expectedResult, _ := time.ParseDuration("683164800s")
+	startTime := time.Date(1997, 10, 16, 0, 0, 0, 0, time.UTC)
+	endTime := time.Date(2019, 06, 10, 0, 0, 0, 0, time.UTC)
+
+	actualResult := CalculateDurationBetweenTime(startTime, endTime)
+
+	if expectedResult != actualResult {
+		t.Errorf("need %v but got %v", expectedResult, actualResult)
+	}
+}
