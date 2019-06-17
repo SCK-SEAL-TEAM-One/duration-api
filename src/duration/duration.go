3e5462cfa78a41d9588156950d2d7b529787db73
uracha <uracha_p@kkumail.com>
2019-06-17 11:07:08 +0700

    Create : สร้างฟังก์ชันและ Unit test ConvertYearMonthDayToTime

--- src/duration/duration.go
+++ src/duration/duration.go
@@ -1,5 +1,11 @@
 package duration
 
+import (
+	"time"
+	api "../api"
+)
 
-
+func ConvertYearMonthDayToTime(yearmonthday api.YearMonthDay) time.Time {
+	return time.Date(yearmonthday.Year,time.Month(yearmonthday.Month),yearmonthday.Day,0,0,0,0,time.UTC)
+}
 
--- src/duration/duration_test.go
+++ src/duration/duration_test.go
@@ -1,1 +1,23 @@
 package duration
+
+import (
+	"time"
+	"testing"
+	api "../api"
+)
+
+func Test_ConvertYearMonthDayToTime_1997_Year_10_Month_16_Day_Should_Time_Year_1997_Month_10_Day_16(t *testing.T){
+	yearmonthday := api.YearMonthDay{
+		Year:1997,
+		Month:10,
+		Day:16,
+	}
+
+	expectedResult := time.Date(1997,10,16,0,0,0,0,time.UTC)
+
+	actualResult := ConvertYearMonthDayToTime(yearmonthday)
+
+	if expectedResult != actualResult {
+		t.Errorf("need %v but got %v",expectedResult,actualResult)
+	}
+}
